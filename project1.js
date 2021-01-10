c48606c13eed36c51cd9aa73df3ad101f109715b
MattG2000 <GuffyMatthewO@JohnDeere.com>
2021-01-09 19:07:51 -0500

    testing pairing function.

--- project1.js
+++ project1.js
@@ -1,1 +1,31 @@
 console.log("it works3")
+//IF innerText of id = the innerText of the second id then do not
+//change color of text back to green
+
+
+//testing selecting and changing card
+
+let card = document.querySelectorAll('.card');
+
+
+// //let square = document.querySelectorAll('.square');
+//         let reset = document.querySelector('#reset');
+
+        card.addEventListener('click', function() {
+            card.style.color = 'white';
+
+        })
+
+
+        function flipped() {
+            card.style.color = 'green';
+
+            
+        }
+        
+        let interval = setInterval(flipped, 5000);
+
+   //testing code above          
+           //}
+           
+           //let interval = setInterval(transitionSlide, 5000);
