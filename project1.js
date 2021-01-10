45cad865d7629dc0d1edd6843b425691926d77ba
MattG2000 <GuffyMatthewO@JohnDeere.com>
2021-01-09 20:12:22 -0500

    debugging.

--- project1.js
+++ project1.js
@@ -5,25 +5,63 @@ console.log("it works3")
 
 //testing selecting and changing card
 
-let card = document.querySelectorAll('.card');
+let card1 = document.querySelector('#card1');
+let card2 = document.querySelector('#card2');
+let card3 = document.querySelector('#card3');
+let card4 = document.querySelector('#card4');
+let card5 = document.querySelector('#card5');
+let card6 = document.querySelector('#card6');
+let card7 = document.querySelector('#card7');
+let card8 = document.querySelector('#card8');
+let card9 = document.querySelector('#card9');
+let card10 = document.querySelector('#card10');
+let card11 = document.querySelector('#card11');
+let card12 = document.querySelector('#card12');
+let clicks = 0;
+let div = document.querySelectorAll('div');
+console.log(div[0]);
+console.log(div);
 
+
+// card1.addEventListener("click", function() {
+//     if (clicks % 2 === 0) {
+//         clicks++
+//         card1.style.color = 'white';
+
+//     }
+//     else if (clicks % 2 === 1) {
+//         clicks--
+//         card1.style.backgroundColor = 'white';
+//     }}
 
 // //let square = document.querySelectorAll('.square');
 //         let reset = document.querySelector('#reset');
+// if (clicks % 2 === 0) {
+//     clicks++
+//     card.style.color = 'white';
 
-        card.addEventListener('click', function() {
-            card.style.color = 'white';
+// }
+// else if (clicks % 2 === 1) {
+//     clicks--
+//     card.style.color = 'white';
+// }
 
-        })
 
 
-        function flipped() {
-            card.style.color = 'green';
+
+        // card.addEventListener('click', function() {
+        //     card.style.color = 'white';
+
+        // })
+
+
+        // function flipped() {
+        //     card.style.color = 'green';
 
             
-        }
+        // }
         
-        let interval = setInterval(flipped, 5000);
+        // let interval = setInterval(flipped, 5000);
 
    //testing code above          
            //}
